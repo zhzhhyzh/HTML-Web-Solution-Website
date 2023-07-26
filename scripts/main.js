9023a7d31852c75cb963a04f7b5eb2a1fbb5dabc
Quinton Pang Yi Xuan <quinton.qpyx@gmail.com>
2023-07-26 12:22:41 +0800

    finalized

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,62 @@
-'use strict';
+var modal = document.getElementById("myModal");
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.assessment_button');
+// Get the button that opens the modal
+var btn = document.getElementById("assessment__button");
 
-const openModal = function () {
-    console.log('Button Click');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+if(btn)
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+if(span)
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-for (let i = 0; i < btnOpenModal.length; i++)
-    btnOpenModal[i].addEventListener('click', openModal);
+if(btn&&span)
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+const onSubmit = () =>{
+   alert("Sent successfully!")
+}
 
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
-document.addEventListener('keydown', function (e) {
-    if (e.code === "Escape") {
-        if (!modal.classList.contains('hidden')){
-            closeModal()
-        }
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
     }
-})
+  }
+});
+$(document).ready( function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
 
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
 
